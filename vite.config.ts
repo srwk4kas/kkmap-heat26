ad0fce45d5eaa7c87db9388fd14e8848d367ebd1
srwk4kas <srwk4kas@gmail.com>
2026-05-26 12:28:36 +0700

    fix: total shift to standard tanstack start config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "@tanstack/react-start/config";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite(),
-    react(),
-    tsconfigPaths(),
-  ],
+  vite: {
+    plugins: [
+      tsconfigPaths(),
+    ],
+  },
 });
