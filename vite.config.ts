258e65add4244d663237021a4222b6a71cdfe82a
srwk4kas <srwk4kas@gmail.com>
2026-05-26 12:30:41 +0700

    fix: delete temporary mjs configs and apply clean build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from "@tanstack/react-start/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  vite: {
-    plugins: [
-      tsconfigPaths(),
-    ],
-  },
+  plugins: [
+    TanStackRouterVite(),
+    react(),
+    tsconfigPaths(),
+  ],
 });
